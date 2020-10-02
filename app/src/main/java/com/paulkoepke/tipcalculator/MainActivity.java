8a1934877c92b1856ce7f50bf0fbee205cfbb487
Paul Koepke <paul.koepke@gmail.com>
2020-10-02 14:30:39 -0500

    Fixed issue where setContentView was missing and findViewById was in the wrong spot, which prevented the app from launching at all. Moved to appcompat support library version 28+.

--- app/src/main/java/com/paulkoepke/tipcalculator/MainActivity.java
+++ app/src/main/java/com/paulkoepke/tipcalculator/MainActivity.java
@@ -2,25 +2,21 @@ package com.paulkoepke.tipcalculator;
 
 import android.annotation.SuppressLint;
 import android.graphics.Color;
-import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.webkit.WebView;
-
-import java.io.Console;
 
 public class MainActivity extends AppCompatActivity {
 
     //getSupportActionBar().setHomeButtonEnabled(true); // Probably unnecessary
     //getSupportActionBar().setTitle("Tip Calculator");
 
-    WebView mainWebView = (WebView) findViewById(R.id.mainWebView);
-
     @SuppressLint("SetJavaScriptEnabled")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        WebView mainWebView = findViewById(R.id.mainWebView);
 
         mainWebView.setBackgroundColor(Color.BLACK);
         mainWebView.getSettings().setJavaScriptEnabled(true);
